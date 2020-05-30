a25a642e759ac473dfac3208368665f12bc40672
Partha <the.saintneo@gmail.com>
2020-05-30 18:47:37 -0400

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Partha Sarathy on 5/30/20.
+//  Copyright © 2020 Partha Sarathy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
